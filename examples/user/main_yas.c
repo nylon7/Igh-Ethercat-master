c4ffe8e367183076bba4b9d6b66966c01d36e451
Nylon7 <nylon7717@gmail.com>
2015-08-01 13:46:42 +0800

    add non-dc yaskawa sevro motor control

---
+++ examples/user/main_yas.c
@@ -0,0 +1,17 @@
+//Yaskawa servo motor
+
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "ecrt.h"
+
+// Application parameters
+#define FREQUENCY 1000
+
+
+
