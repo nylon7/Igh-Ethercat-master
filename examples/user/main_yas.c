9a49c30bcce9285d5417378ebec213563d94ec54
Nylon7 <nylon7717@gmail.com>
2015-08-01 14:19:20 +0800

    add pdo entry

--- examples/user/main_yas.c
+++ examples/user/main_yas.c
@@ -14,12 +14,12 @@
 #define FREQUENCY 1000
 
 //yaswawa vendor_id & product_code
-#define yas             1,0
-#define yaskawa         0x00000539, 0x02200001
+#define yas             0
+#define yaskawa         0x02200001
 
 static ec_master_t *master = NULL;
-static ec_domain_t *domain_w = NULL;
 static ec_domain_t *domain_r = NULL;
+static ec_domain_t *domain_w = NULL;
 
 static ec_slave_config_t *sc  = NULL;
 
@@ -29,8 +29,53 @@ static unsigned int sig_alarms = 0;
 static unsigned int user_alarms = 0;
 
 // process data
-static uint8_t *domain_w_pd = NULL;
 static uint8_t *domain_r_pd = NULL;
+static uint8_t *domain_w_pd = NULL;
+
+//
+// offsets for PDO entries
+static unsigned int ctrl_word   ;
+static unsigned int target_pos  ;
+static unsigned int tar_velo    ;
+static unsigned int tar_torq    ;
+static unsigned int max_torq    ;
+static unsigned int modeofoper  ;
+static unsigned int interpolateddata ;
+
+static unsigned int status_word ;
+static unsigned int actual_pos  ;
+static unsigned int torq_actu_val;
+static unsigned int following_actu_val;
+static unsigned int modeofop_display;
+static unsigned int touch_probe_stat;
+static unsigned int touch_probe_val;
+
+static signed long temp[8]={};
+
+//rx pdo entry of  motor
+const static ec_pdo_entry_reg_t domain_r_regs[] = 
+{
+        {yas,yaskawa,0x6040,00,&ctrl_word              },
+        {yas,yaskawa,0x607a,00,&target_pos             },
+        {yas,yaskawa,0x60ff,00,&tar_velo               },
+        {yas,yaskawa,0x6071,00,&tar_torq               },
+        {yas,yaskawa,0x6072,00,&max_torq               },
+        {yas,yaskawa,0x6060,00,&modeofoper             },
+        {yas,yaskawa,0x60c1,01,&interpolateddata       },
+	{}
+};
+
+const static ec_pdo_entry_reg_t domain_w__regs[] = 
+{
+        {yas,yaskawa,0x6041,00,&status_word            },
+        {yas,yaskawa,0x6064,00,&actual_pos             },
+        {yas,yaskawa,0x6077,00,&torq_actu_val          },
+        {yas,yaskawa,0x60f4,00,&following_actu_val     },
+        {yas,yaskawa,0x6061,00,&modeofop_display       },
+        {yas,yaskawa,0x60b9,00,&touch_probe_stat       },
+        {yas,yaskawa,0x60ba,00,&touch_probe_val        },
+ 	{}
+};
 
 //signal to turn off servo on state
 static unsigned int servo_flag;
