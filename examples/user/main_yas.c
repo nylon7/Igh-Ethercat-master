96e822b1d90a22dd6a9bd24972ba0e0d53721181
Nylon7 <nylon7717@gmail.com>
2015-08-01 14:07:58 +0800

    yaskawa init

--- examples/user/main_yas.c
+++ examples/user/main_yas.c
@@ -13,5 +13,29 @@
 // Application parameters
 #define FREQUENCY 1000
 
+//yaswawa vendor_id & product_code
+#define yas             1,0
+#define yaskawa         0x00000539, 0x02200001
+
+static ec_master_t *master = NULL;
+static ec_domain_t *domain_w = NULL;
+static ec_domain_t *domain_r = NULL;
+
+static ec_slave_config_t *sc  = NULL;
 
 
+// Timer
+static unsigned int sig_alarms = 0;
+static unsigned int user_alarms = 0;
+
+// process data
+static uint8_t *domain_w_pd = NULL;
+static uint8_t *domain_r_pd = NULL;
+
+//signal to turn off servo on state
+static unsigned int servo_flag;
+
+float move_value = 0;
+
+static unsigned int counter = 0;
+static unsigned int blink = 0;
